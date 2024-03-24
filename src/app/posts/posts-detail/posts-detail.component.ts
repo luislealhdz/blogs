9f4bd86859312bd0dd83b33d9d84b4c2decf4cf3
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-03-24 16:02:37 -0600

    first commit

---
+++ src/app/posts/posts-detail/posts-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './posts-detail.component.html',
+  styleUrl: './posts-detail.component.scss'
+})
+export class PostsDetailComponent {
+
+}
---
+++ src/app/posts/posts.routes.ts
@@ -0,0 +1,3 @@
+import { Routes } from '@angular/router';
+
+export const POSTS_ROUTES: Routes = [];
