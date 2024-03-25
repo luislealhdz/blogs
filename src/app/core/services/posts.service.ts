67f3d091dec66b021e65e43ab2496d57a84f0bb0
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-03-25 12:31:49 -0600

    feat(Component): :sparkles: Display posts list
    The postService service was created to obtain the posts and render them in the posts list

--- src/app/core/services/posts.service.ts
+++ src/app/core/services/posts.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  onFetchPosts<Post>(): Observable<Post[]> {
+    return this.http.get<Post[]>(environment.API_POSTS);
+  }
 }
--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { PostsService } from '../../core/services/posts.service';
+import Post from '../../core/models/post';
 
 @Component({
   selector: 'app-posts-list',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.scss'
 })
-export class PostsListComponent {
+export class PostsListComponent implements OnInit {
+  postsList!: Post[];
 
+  postsService = inject(PostsService);
+
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+
+  fetchPosts(): void {
+    this.postsService.onFetchPosts<Post>().subscribe(post => {
+      this.postsList = post;
+    });
+  }
 }
